148068511204f704f9c9face74b19fb12233b2e5
000 <10037022+hlzzxx@user.noreply.gitee.com>
2022-10-14 10:36:25 +0800

    push dist

---
+++ dist/assets/index.b398e87c.js
@@ -0,0 +1,1 @@
+import{d as o,b as t,c as n,k as s,G as r}from"./index.daa6cca7.js";const a={class:"tag"},m=o({__name:"index",setup(c){return(_,p)=>{const e=r("router-view");return t(),n("div",a,[s(e)])}}});export{m as default};
